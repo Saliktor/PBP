2bd6e2edf8864517ea1ee3dc896c03f115790de9
Austin La Comb <austin.lacomb@gmail.com>
2018-02-08 16:36:48 -0500

    Converted User to Hibernate

--- PBP-Project/src/main/java/com/revature/beans/User.java
+++ PBP-Project/src/main/java/com/revature/beans/User.java
@@ -1,19 +1,31 @@
 package com.revature.beans;
 
-import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="bear")
 public class User {
+	@Id
+	@SequenceGenerator(name="user_pk",sequenceName="userid_seq", allocationSize=1)
+	@GeneratedValue(generator="user_pk", strategy=GenerationType.SEQUENCE)
+	int id;
 	String firstname;
 	String lastname;
 	String username;
 	String password;
 	String email;
-	int id;
-	boolean isAdmin;
-	boolean isBanned;
-	boolean isMuted;
+	int isAdmin; 		// 0 for no, 1 for yes
+	int isBanned;		// 0 for no, 1 for yes
+	int isMuted;		// 0 for no, 1 for yes
 	
-	HashSet<Integer> createdThreads;
-	HashSet<Post> createdPost;
-	HashSet<Post> repliedPost;
+	Set<Integer> createdThreads;
+	Set<Post> createdPost;
+	Set<Post> repliedPost;
 }
