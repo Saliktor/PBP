f0c537e6affdaa84e8ec67f214eee47b1278a9fd
Ahmad Naser <naser_ahmad@ymail.com>
2018-02-11 18:47:25 -0500

    angular

---
+++ angular-pbp/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserService } from './user.service';
+import { AppRoutingModule } from './app-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular-pbp/src/app/user.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { User } from './user';
+import { CurrentUser } from './current-user';
+import { Headers, Http } from '@angular/http';
+
+import { Observable } from 'rxjs/observable';
+import { of } from 'rxjs/observable/of';
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class UserService {
+  private appUrl = 'http://localhost:8080/PBP/login';
+  private headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded'});
+  private user: User;
+
+  constructor(private http: Http) { }
+
+  login(username: string, password: string) {
+    if (username && password) {
+      const body = `username=${username}&password=${password}`;
+      return this.http.post(this.appUrl, body, { headers: this.headers, withCredentials: true })
+        .map(
+        resp => {
+          console.log(resp);
+          const user: CurrentUser = resp.json() as CurrentUser;
+          this.user = user.user;
+          return user;
+        }
+        );
+    } else {
+      return this.http.get(this.appUrl, {headers: this.headers, withCredentials: true })
+      .map(
+        resp => {
+          const user: CurrentUser = resp.json() as CurrentUser;
+          this.user = user.user;
+          return user;
+        }
+      );
+    }
+
+  }
+}
