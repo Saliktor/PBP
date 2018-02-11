0b6c5fb5fec4fbd52ff5d02a735959d74b50301a
shawn gavin <saliktor@gmail.com>
2018-02-10 22:38:35 -0500

    Created a lib folder for the WEB-INF to hold the ojdbc.jar. Fixes issue of when deployed to tomcat not being able to find the ojdbc class file. Also am able to successfully test creating a user getting input for username, password and email from console

--- PBP-Project/src/main/java/com/revature/TestingStuff/Test.java
+++ PBP-Project/src/main/java/com/revature/TestingStuff/Test.java
@@ -1,21 +1,37 @@
 package com.revature.TestingStuff;
 
-//import org.apache.log4j.Logger;
-//import org.hibernate.Session;
-//import org.hibernate.Transaction;
-//
-//import com.revature.beans.UserAccount;
-//import com.revature.dao.UserDAO;
-//import com.revature.dao.UserDAOImp;
-//import com.revature.util.HibernateUtil;
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+
+import com.revature.dao.UserDAO;
+import com.revature.dao.UserDAOImp;
+import com.revature.services.UserService;
+import com.revature.util.HibernateUtil;
 
 public class Test {
-//
-//	private static HibernateUtil hu = HibernateUtil.getInstance();
-//	private static Logger log = Logger.getLogger(Test.class);
-//	private static UserDAO uDAO = new UserDAOImp();
-//	
-//	public static void main(String[] args) {
-//		
-//	}
+
+	private static HibernateUtil hu = HibernateUtil.getInstance();
+	private static Logger log = Logger.getLogger(Test.class);
+	private static UserDAO uDAO = new UserDAOImp();
+	private static UserService userService = new UserService();
+	private static Scanner reader = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+
+		
+	}
+	
+	public void testCreateUser() {
+		System.out.println("Enter username: ");
+		String username = reader.nextLine();
+		System.out.println("Enter password: ");
+		String password = reader.nextLine();
+		System.out.println("Enter email: ");
+		String email = reader.nextLine();
+		
+		userService.createUser(username, password, email);
+
+	}
+
 }
--- PBP-Project/src/main/java/com/revature/servlets/HelloServlet.java
+++ PBP-Project/src/main/java/com/revature/servlets/HelloServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.revature.services.UserService;
+import com.revature.TestingStuff.Test;
 
 public class HelloServlet extends HttpServlet{
 	/**
@@ -15,23 +16,11 @@ public class HelloServlet extends HttpServlet{
 	 */
 	private static final long serialVersionUID = 1L;
 	private UserService userService = new UserService();
+	private Test testClass = new Test();
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
-		testCreatingUser();
+		testClass.testCreateUser();
 		resp.getWriter().write("Hello World");
 	}
 	
-	private void testCreatingUser(){
-		Scanner reader = new Scanner(System.in);
-		System.out.println("Enter username: ");
-		String username = reader.nextLine();
-		System.out.println("Enter password: ");
-		String password = reader.nextLine();
-		System.out.println("Enter email: ");
-		String email = reader.nextLine();
-		
-		userService.createUser(username, password, email);
-		
-		reader.close();
-	}
 }
