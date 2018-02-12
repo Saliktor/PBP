bc1616e3e17e7b8b1fbe3ec176f6d8ddf6a3566f
shawn gavin <saliktor@gmail.com>
2018-02-12 13:46:20 -0500

    Changes to the testing class

--- PBP-Project/src/main/java/com/revature/TestingStuff/Test.java
+++ PBP-Project/src/main/java/com/revature/TestingStuff/Test.java
@@ -2,23 +2,14 @@ package com.revature.TestingStuff;
 
 import java.util.Scanner;
 
-import org.apache.log4j.Logger;
-
-import com.revature.dao.UserDAO;
-import com.revature.dao.UserDAOImp;
-import com.revature.services.UserService;
-import com.revature.util.HibernateUtil;
+import com.revature.services.UserServiceImp;
 
 public class Test {
 
-	private static HibernateUtil hu = HibernateUtil.getInstance();
-	private static Logger log = Logger.getLogger(Test.class);
-	private static UserDAO uDAO = new UserDAOImp();
-	private static UserService userService = new UserService();
+	private static UserServiceImp userService = new UserServiceImp();
 	private static Scanner reader = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-
 		
 	}
 	
