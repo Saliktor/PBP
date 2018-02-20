9bc3cb65e756cfdc8801ee7acee8b964a39cfde2
Shawn Gavin <saliktor@Yahoo.com>
2018-02-19 21:06:39 -0500

    Merged WEB-INF/beans.xml and UserService

--- PBP-Project/src/main/java/com/revature/services/UserService.java
+++ PBP-Project/src/main/java/com/revature/services/UserService.java
@@ -6,14 +6,15 @@ import com.revature.beans.Player;
 import com.revature.beans.UserAccount;
 
 public interface UserService {
+	public UserAccount editUser(UserAccount user);
+
 	public UserAccount createUser(String username, String password, String email);
 	public UserAccount createAdmin(String username, String password, String email);
 	
 	public UserAccount getUser(String username, String password);
-
 	public Player getPlayer(int id);
 	public Set<Player> getPlayers(UserAccount user);
 	
-	public UserAccount editUser(UserAccount user);
+
 	
 }
--- PBP-Project/src/main/java/com/revature/services/UserServiceImp.java
+++ PBP-Project/src/main/java/com/revature/services/UserServiceImp.java
@@ -4,22 +4,30 @@ package com.revature.services;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
-import org.springframework.stereotype.Component;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import org.springframework.stereotype.Service;
+
 
 import com.revature.beans.Player;
 import com.revature.beans.UserAccount;
-import com.revature.dao.GameDAOImp;
+import com.revature.dao.GameDAO;
 import com.revature.dao.PlayerDAO;
-import com.revature.dao.PlayerDAOImp;
-import com.revature.dao.UserDAOImp;
+import com.revature.dao.UserDAO;
 
-@Component
+
+@Service
 public class UserServiceImp implements UserService {
 	static final Logger log = Logger.getLogger(UserServiceImp.class);
 	
-	private static UserDAOImp userDAO = new UserDAOImp();
-	private static PlayerDAO playerDAO = new PlayerDAOImp();
-	private static GameDAOImp gameDAO = new GameDAOImp();
+	private static ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+	private static PlayerDAO playerDAO = ac.getBean(PlayerDAO.class);
+	private static GameDAO gameDAO = ac.getBean(GameDAO.class);
+	@Autowired
+	private UserDAO userDAO;
 	
 	/*
 	 * Creates a user object based on passed parameters
@@ -49,12 +57,16 @@ public class UserServiceImp implements UserService {
 
 	}
 	
+	
+	/* Creates a new admin account. Not updated, do not use without testing */
 	public UserAccount createAdmin(String username, String password, String email) {
 		UserAccount user = createUser(username, password, email);
 		user.setIsAdmin(1);
 		return user;
 	}
 	
+	
+	/* Retrieve a user account from database with passed parameters */
 	public UserAccount getUser(String username, String password) {
 		UserAccount user = new UserAccount();
 		user.setUsername(username);
@@ -69,18 +81,23 @@ public class UserServiceImp implements UserService {
 		
 		return user;
 	}
-
-	public Player getPlayer(int id) {
 		
+
+	/* Retrieve a player from database with passed player id */
+	public Player getPlayer(int id) {
 		Player player = playerDAO.getPlayer(id);
 		return player;
 	}
 	
+	
+	/* Retrieve a set of players attached to a user account*/
 	public Set<Player> getPlayers(UserAccount user){
 		Set<Player> players = gameDAO.getUserPlayersAndGames(user);
 		return players;
 	}
-
+	
+	
+	/* Update the user account to the database */
 	public UserAccount editUser(UserAccount user) {
 		return userDAO.updateUser(user);
 	}
