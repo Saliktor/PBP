8998e4940a6c4126a5a4d76cddabcbc85fa77c2d
Ahmad Naser <naser_ahmad@ymail.com>
2018-02-20 15:04:22 -0500

    messageDAO

--- PBP-Project/src/main/java/com/revature/dao/MessageDAOImp.java
+++ PBP-Project/src/main/java/com/revature/dao/MessageDAOImp.java
@@ -14,9 +14,12 @@ import org.springframework.stereotype.Repository;
 import com.revature.beans.Game;
 import com.revature.beans.Message;
 
+
 @Repository
 public class MessageDAOImp implements MessageDAO, HibernateSession {	
 	private Session session;
+	private static final String timePosted = "timePosted";
+
 	
 	public void setSession(Session session) {
 		this.session = session;
@@ -34,9 +37,9 @@ public class MessageDAOImp implements MessageDAO, HibernateSession {
 	public Message getLatestMessage() {
 		Message message = null;
 		DetachedCriteria maxTime = DetachedCriteria.forClass(Message.class);
-		maxTime.setProjection(Projections.max("timePosted"));
+		maxTime.setProjection(Projections.max(timePosted));
 		Criteria query = session.createCriteria(Message.class);
-		query.add(Property.forName("timePosted").eq(maxTime));
+		query.add(Property.forName(timePosted).eq(maxTime));
 		message = (Message) query.uniqueResult();
 		return message;
 	}
@@ -52,7 +55,7 @@ public class MessageDAOImp implements MessageDAO, HibernateSession {
 		List<Message> messages = null;
 		messages = session.createCriteria(Message.class)
 				.add(Restrictions.eq("game", game))
-				.add(Restrictions.ge("timePosted", timestamp)).list();
+				.add(Restrictions.ge(timePosted, timestamp)).list();
 		return messages;
 
 	}
