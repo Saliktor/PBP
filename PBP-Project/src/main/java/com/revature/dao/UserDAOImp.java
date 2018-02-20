97bbf51e2946328eeb6b136f9a31600e4514fdf5
Shawn Gavin <saliktor@Yahoo.com>
2018-02-19 21:16:15 -0500

    Merged UserDAO

--- PBP-Project/src/main/java/com/revature/dao/UserDAOImp.java
+++ PBP-Project/src/main/java/com/revature/dao/UserDAOImp.java
@@ -3,124 +3,69 @@ package com.revature.dao;
 import org.apache.log4j.Logger;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
 
 import com.revature.beans.UserAccount;
-import com.revature.util.HibernateUtil;
 
-public class UserDAOImp implements UserDAO {
+@Repository
+public class UserDAOImp implements UserDAO, HibernateSession {
 
-	private static HibernateUtil hu = HibernateUtil.getInstance();
 	private static Logger log = Logger.getLogger(UserDAOImp.class);
-	
+
+	private Session session;
+
+	public void setSession(Session session) {
+		this.session = session;
+
+	}
 
 	public boolean createUser(UserAccount user) {
-		Session session = hu.getSession();
-		Transaction tx = null;
-		try{
-			tx = session.beginTransaction();
-			log.trace("Begun transaction");
-			log.trace(session.save(user));
-			tx.commit();
-			return true;
-		} catch(Exception e) {
-			if(tx!=null) {
-				tx.rollback();
-				log.error("Exception occured while creating user", e);
-				return false;
-			}
-		} finally {
-			session.close();
-		}
-		return false;
+		session.save(user);
+		return true;
 	}
-
 
 	public UserAccount getUser(UserAccount user) {
-		Session session = hu.getSession();
+		log.trace("USER ACCOUNT CALLED!");
 		UserAccount newUser = null;
-		try {
-			Criteria criteria = session.createCriteria(UserAccount.class);
-			criteria.add(Restrictions.eq("username", user.getUsername()))
-			.add(Restrictions.eq("password", user.getPassword()));
-			
-			newUser = (UserAccount) criteria.uniqueResult();
-			
-			
-		} catch (Exception e) {
-			log.trace(e);
-		} finally {
-			session.close();
-		}
-		
-		return newUser;		
+
+		Criteria criteria = session.createCriteria(UserAccount.class);
+		criteria.add(Restrictions.eq("username", user.getUsername()))
+				.add(Restrictions.eq("password", user.getPassword()));
+
+		newUser = (UserAccount) criteria.uniqueResult();
+
+		return newUser;
 	}
-	
 
 	public boolean isEmailAvailable(String email) {
-		Session session = hu.getSession();
 		UserAccount newUser = null;
-		try {
-			Criteria criteria = session.createCriteria(UserAccount.class);
-			criteria.add(Restrictions.eq("email", email));
-			
-			newUser = (UserAccount) criteria.uniqueResult();
-			if(newUser == null)
-				return true;
 
-		} catch (Exception e) {
-			log.trace(e);
-			return false;
-		} finally {
-			session.close();
-		}
+		Criteria criteria = session.createCriteria(UserAccount.class);
+		criteria.add(Restrictions.eq("email", email));
+
+		newUser = (UserAccount) criteria.uniqueResult();
+		if (newUser == null)
+			return true;
+
 		return false;
 	}
-
 
 	public boolean isUsernameAvailable(String username) {
-		Session session = hu.getSession();
 		UserAccount newUser = null;
-		try {
-			Criteria criteria = session.createCriteria(UserAccount.class);
-			criteria.add(Restrictions.eq("username", username));
-			
-			newUser = (UserAccount) criteria.uniqueResult();
-			if(newUser == null)
-				return true;
+		log.trace(session);
+		Criteria criteria = session.createCriteria(UserAccount.class);
+		criteria.add(Restrictions.eq("username", username));
 
-		} catch (Exception e) {
-			log.trace(e);
-			return false;
-		} finally {
-			session.close();
-		}
+		newUser = (UserAccount) criteria.uniqueResult();
+		if (newUser == null)
+			return true;
+
 		return false;
 	}
-
 
 	public boolean deleteUser(int userId) {
-		
+
 		return false;
 	}
-
-
-	public UserAccount updateUser(UserAccount user) {
-		Session session = hu.getSession();
-		UserAccount updatedUser = null;
-		try{
-			user = (UserAccount) session.merge(user);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			return user;
-		} finally {
-			session.close();
-		}
-		return user;
-	}
-
-
-
 }
